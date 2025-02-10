7c745b13d89d79c5830d4e1403d98f6e8e954d6b
THALLAPALLY SREEPOORNA <sreepoorna07@gmail.com>
2025-02-10 12:05:57 +0530

    sentiment

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request  # type: ignore
+app = Flask(_name_)
 @app.route("/")
 def Home():
     return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run()
+@app.route("/predict",methods= ["GET","POST"])
+def predict():
+    if request.method== "POST":
+        msg = request.form.get("message")
+        print(msg)
+    else:
+        return render_template("predict.html")
+if _name_ == "_main_":
+    app.run(host="0.0.0.0",port=5050)
